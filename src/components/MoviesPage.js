9656f88e7f99a8733b072e29d38ebf9d3db79319
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-05-02 11:55:42 +0300

    done

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { Route } from "react-router-dom";
 import MoviesList from "./MoviesList";
 
@@ -6,6 +5,7 @@ function MoviesPage({ movies }) {
   return (
     <div>
       <MoviesList movies={movies} />
+      <Route movies={movies}/>
     </div>
   );
 }
